0646e7a4cb8e81b1caab4a4268e4d20e44a27cc5
Gilberto Junior <juninhossj4@gmail.com>
2023-03-18 21:28:51 -0300

    Second Commit

--- login_screen.dart
+++ login_screen.dart
@@ -8,25 +8,13 @@ class LoginScreen extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Container(
-          width: double.infinity,
-          height: double.infinity,
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage("assets/book.png"), fit: BoxFit.cover),
-          ),
-          child: const Center(
-              child: Text(
-            "My Books",
-            style: TextStyle(
-              color: Color.fromARGB(255, 101, 71, 59),
-              fontSize: 40,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2,
-              height: -8,
+            width: double.infinity,
+            height: double.infinity,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/book2.png"), fit: BoxFit.cover),
             ),
-           )
-          ),
-        ),
+            child: const Center()),
       ),
     );
   }
